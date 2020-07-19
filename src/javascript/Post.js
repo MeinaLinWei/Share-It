b20e11d84e5cd82580c2213ecd3f42d969f7a10b
MeinaLinWei <meina.lw12@gmail.com>
2020-07-20 02:48:04 +0400

    Add post button

--- src/javascript/Post.js
+++ src/javascript/Post.js
@@ -8,6 +8,10 @@ function Post({postId, username, caption, imageUrl}) {
     const [comments, setComments] = useState([]);
     const [comment, setComment] = useState([]);
 
+    const postComment = (event ) => {
+
+    }
+
     useEffect(() => {
         let unsubscribe;
 
@@ -42,26 +46,36 @@ function Post({postId, username, caption, imageUrl}) {
                 {/* Username + Caption*/}
                 <h4 className="post__text"><strong>{username}</strong>: {caption}</h4>
 
-                <div className="post__comment-input">
+                <div className="post__comment">
                     <form>
-                        <input 
-                        className="post__comment-input"
-                        type="text"
-                        placeholder="Add a comment..."
-                        value={comment}
-                        onChange={(event) => setComment(event.target.value)}
+                        <input
+                            className="post__comment-input"
+                            type="text"
+                            placeholder="Add a comment..."
+                            value={comment}
+                            onChange={(event) => setComment(event.target.value)}
                         />
-                    </form>
-                    <Button
-                        className="post__comment-button"
-                        disabled={!comment}
-                        type="submit"
-                        onClick={postComment}
-                    >
+                        <Button
+                            style={{
+                                fontSize: "15px",
+                                color: "#314455",
+                            }}
+                            className="post__comment-button"
+                            disabled={!comment}
+                            type="submit"
+                            onClick={postComment}
+                        >
                         Post
                     </Button>
-
+                    </form>
                 </div>
+                <div className="post__comments__all">
+                        {comments.map((comment) => (
+                            <p>
+                                <b>{comment.username}</b> {comment.text}
+                            </p>
+                        ))}
+                    </div>
             </div>
             
 
